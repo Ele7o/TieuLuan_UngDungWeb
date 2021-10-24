5c826e385f9ec7fa568edf99358b6240fc5cef24
Erik <superkokono@gmail.com>
2021-10-24 21:40:48 +0700

    v0.1.0
    Upload BaseFile

--- js/detail1.js
+++ js/detail1.js
@@ -1,12 +1,21 @@
+// $(document).ready(function(){
+//     let txt = "";
+//     var image = document.createElement("img");
+//     for(let i = 0; i <24; i++){
+//         txt = i;
+//         if(i<10){
+//             txt = "0" + i;
+//         }
+//         img = image.setAttribute("src", "../images/manga" + txt + ".jpg");
+//         document.getElementById("imgdiv").appendChild(img);
+//     }
+// });
 $(document).ready(function(){
-    let txt = "";
-    var image = document.createElement("img");
-    for(let i = 0; i <24; i++){
-        txt = i;
-        if(i<10){
-            txt = "0" + i;
+    $('#chapterSelectRun').on('change', function(){
+        var url = $(this).val();
+        if(url){
+            window.location = url;
         }
-        img = image.setAttribute("src", "../images/manga" + txt + ".jpg");
-        document.getElementById("imgdiv").appendChild(img);
-    }
+        return false;
+    })
 });
